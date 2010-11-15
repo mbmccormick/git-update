a1af7be3909c98657a7edb395ba785f528cfcea1
Matt McCormick <mbmccormick@gmail.com>
2010-11-15 18:09:36 -0500

    Added config file.

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
+    include "config.php";
+
     $out = array();
     exec("git pull origin master", $out);
 
     echo "<code>\n";
+    echo "$ cd " . $RepositoryPath;
     echo "$ git pull origin master<br />\n";
+    echo "<br />\n";
     foreach($out as $line)
     {
         echo str_replace("\n", "<br />", $line) . "<br />\n";
