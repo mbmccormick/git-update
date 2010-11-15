f69f34bb909c47571b2cab961184ac2d5e0b0cbe
Matt McCormick <mbmccormick@gmail.com>
2010-11-15 18:19:55 -0500

    Added quotes character.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     exec("cd \"$RepositoryPath\"; git pull origin master", $out);
 
     echo "<code>\n";
-    echo "$ cd " . $RepositoryPath . "<br />\n";
+    echo "$ cd \"" . $RepositoryPath . "\"<br />\n";
     echo "$ git pull origin master<br />\n";
     echo "<br />\n";
     foreach($out as $line)
